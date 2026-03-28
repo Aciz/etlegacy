e22b4fad54f8928a71cac30d1fbed34178ce9591
Aranud <aranud66@live.fr>
2026-03-28 11:16:50 +0100

    ui: add missing ui command console (#3360)
    * ui: add missing ui command console
    
    Create an array holding ui commands and parse it to either initalise the list to autocompletion and execution.
    
    * ui: fix function signature unrecognized under win
    
    * cgame: fix incorrect preprocecor define for EDV

--- src/ui/ui_main.c
+++ src/ui/ui_main.c
@@ -1009,7 +1009,7 @@ static void Text_Paint_LimitY(float *maxY, float x, float y, float scale, vec4_t
 /**
  * @brief UI_ShowPostGame
  */
-void UI_ShowPostGame()
+void UI_ShowPostGame(void)
 {
 	trap_Cvar_Set("cg_thirdPerson", "0");
 	trap_Cvar_Set("sv_killserver", "1");
@@ -1497,7 +1497,7 @@ void UI_LoadMenus(const char *menuFile, qboolean reset)
 #ifdef FEATURE_MULTIVIEW
 	trap_PC_AddGlobalDefine("FEATURE_MULTIVIEW");
 #endif
-#ifdef FEATURE_MULTIVIEW
+#ifdef FEATURE_EDV
 	trap_PC_AddGlobalDefine("FEATURE_EDV");
 #endif
 	trap_PC_AddGlobalDefine(va("__WINDOW_WIDTH %f", (uiInfo.uiDC.glconfig.windowAspect / RATIO43) * 640));
@@ -8897,11 +8897,7 @@ void UI_Init(int etLegacyClient, int clientVersion)
 	Q_strncpyz(translated_yes, DC->translateString("Yes"), sizeof(translated_yes));
 	Q_strncpyz(translated_no, DC->translateString("NO"), sizeof(translated_no));
 
-	trap_AddCommand("campaign");
-	trap_AddCommand("listcampaigns");
-
-	trap_AddCommand("listfavs");
-	trap_AddCommand("removefavs");
+	UI_InitConsoleCommand();
 }
 
 /**
@@ -9445,9 +9441,17 @@ static void UI_StartServerRefresh(qboolean full)
  */
 void UI_Campaign_f(void)
 {
-	char           str[MAX_TOKEN_CHARS];
-	int            i;
-	campaignInfo_t *campaign = NULL;
+	char            str[MAX_TOKEN_CHARS];
+	int             i;
+	campaignInfo_t  *campaign = NULL;
+	uiClientState_t cstate;
+
+	trap_GetClientState(&cstate);
+	if (cstate.connState != CA_DISCONNECTED)
+	{
+		Com_Printf("Cannot parse UI campaign while connected to a server\n");
+		return;
+	}
 
 	UI_LoadArenas();
 	UI_MapCountByGameType(qfalse);
@@ -9492,7 +9496,15 @@ void UI_Campaign_f(void)
  */
 void UI_ListCampaigns_f(void)
 {
-	int i, mpCampaigns = 0;
+	int             i, mpCampaigns = 0;
+	uiClientState_t cstate;
+
+	trap_GetClientState(&cstate);
+	if (cstate.connState != CA_DISCONNECTED)
+	{
+		Com_Printf("Cannot parse UI list campaign while connected to a server\n");
+		return;
+	}
 
 	UI_LoadArenas();
 	UI_MapCountByGameType(qfalse);
